5a0bfe056a0f8b8fefa74416c4df18c1ac0d3086
Albert Ruan <berty.ruan@gmail.com>
2022-12-18 17:42:56 +0530

    creates annimation

--- watermark.js
+++ watermark.js
@@ -1,6 +1,49 @@
-function draw() {
-    const ctx = document.getElementById('watermark').getContext('2d');
-    ctx.fillStyle = "red";
-    ctx.font = "28px sans"
-    ctx.fillText("Rawr", 0, 50);
+function init() {
+    window.requestAnimationFrame(draw);
 }
+
+class Text {
+    constructor(content) {
+        this.x = 0;
+        this.y = 0;
+        this.xv = 1;
+        this.yv = 1;
+        this.font = {
+            size: 28,
+            family: 'sans'
+        }
+        this.color = 'red';
+        this.content = content;
+        if (this.y < this.font.size) {
+            this.y = this.font.size;
+        }
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.font = `${this.font.size}px ${this.font.family}`;
+        ctx.fillText(this.content, this.x, this.y);
+        this.move('x');
+        this.move('y');
+    }
+
+    move(axis) {
+        if(this.x <= 255) {
+            this[axis] += this.xv;
+        } else {
+            this[axis] = -1 * ctx.measureText(this.content).width;
+        }
+    }
+}
+
+const ctx = document.getElementById("watermark").getContext("2d");
+const rawr = new Text('rawr');
+
+function draw() {
+    ctx.clearRect(0,0,225,225);
+    rawr.draw();
+
+    window.requestAnimationFrame(draw);
+}
+
+init();
